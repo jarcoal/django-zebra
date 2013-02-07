b1b83369ff12b1f1aa4a41707386fecc5c524193
Jared <jarcoal@gmail.com>
2013-02-06 16:21:49 -0800

    stripe checkout templatetag

--- zebra/templatetags/zebra_tags.py
+++ zebra/templatetags/zebra_tags.py
@@ -28,3 +28,31 @@ def zebra_head_and_stripe_key(context):
 @register.inclusion_tag('zebra/_basic_card_form.html', takes_context=True)
 def zebra_card_form(context):
     return _set_up_zebra_form(context)
+
+
+@register.inclusion_tag('zebra/_stripe_checkout_script.html')
+def zebra_checkout_script(name=None, description=None, image=None, amount=None, label=None, panel_label=None, address=None):
+	"""
+	Renders the Stripe checkout form.
+	https://stripe.com/docs/checkout
+	"""
+
+	#these values have no defaults in the interface
+	attrs = {
+		'name': name,
+		'description': description,
+		'image': image,
+		'amount': amount,
+	}
+
+	#these values do, so we only insert them into context if they've been populated.
+	if label is not None:
+		attrs['label'] = label
+
+	if panel_label is not None:
+		attrs['panel-label'] = panel_label
+
+	if address is not None:
+		attrs['address'] = address
+
+	return { 'attrs': attrs, 'STRIPE_PUBLISHABLE': options.STRIPE_PUBLISHABLE }
